6711568b9939c595990598a57fd4fe7a2d89ec22
godzy <edoardo.venturini@gmail.com>
2013-02-12 09:55:37 +0100

    granatum-model (under experimen folder) removed

--- experiment/granatum-model/src/test/java/eu/granatum/importer/model/TwitterModelTestCase.java
+++
@@ -1,8 +0,0 @@
-package eu.granatum.importer.model;
-
-/**
- * TODO
- * @author Matteo Moci ( matteo (dot) moci (at) gmail (dot) com )
- */
-public class TwitterModelTestCase extends InMemoryNeo4jGraphDbServiceProvider {
-}
